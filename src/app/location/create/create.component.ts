49c537c692a3785d1c1b4c6be9b366eb7daf8db1
Esneider Serna <esneider.marulanda@gmail.com>
2018-09-21 06:55:57 -0500

    .

--- src/app/location/create/create.component.ts
+++ src/app/location/create/create.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LocationModel } from '../../shared/models/location.model';
 import { LocationService } from '../../shared/services/location.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create',
@@ -12,7 +13,8 @@ export class CreateComponent implements OnInit {
   public location: LocationModel = new LocationModel();
 
   constructor(
-    private _locationService: LocationService
+    private _locationService: LocationService,
+    private _roter: Router
   ) { }
 
   ngOnInit() {
@@ -20,6 +22,8 @@ export class CreateComponent implements OnInit {
 
   onSubmit(){
     this._locationService.insertLocation(this.location);
+    this._roter.navigate(['/']);
+
   }
 
 }
